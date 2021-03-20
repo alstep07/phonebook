d6681bd4daa426ace82c92b9663b78bac8ea315a
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-03-20 21:13:17 +0200

    add middleware

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const http = require('http');
 const express = require('express');
 const { request, response } = require('express');
 
-const app = express();
-
 let persons = [
 	{
 		name: 'Arto Hellas',
@@ -27,6 +25,18 @@ let persons = [
 	}
 ];
 
+const requestLogger = (request, response, next) => {
+	console.log('Method:', request.method);
+	console.log('Path:  ', request.path);
+	console.log('Body:  ', request.body);
+	console.log('---');
+	next();
+};
+
+const app = express();
+app.use(express.json());
+app.use(requestLogger);
+
 app.get('/api/persons', (request, response) => {
 	response.json(persons);
 });
@@ -41,6 +51,7 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
 	const id = Number(request.params.id);
+	console.log(request);
 	const person = persons.find((person) => person.id === id);
 	if (!person) {
 		return response.status(400).end();
@@ -53,6 +64,41 @@ app.delete('/api/persons/:id', (request, response) => {
 	persons = persons.filter((person) => person.id !== id);
 	response.status(204).end();
 });
+
+const generateUniqId = () => {
+	const ids = persons.map((person) => person.id);
+	let id = Math.floor(Math.random() * 1000000);
+	while (ids.includes(id)) {
+		id++;
+	}
+	return id;
+};
+
+const validateName = (name) => name && !persons.map((person) => person.name).includes(name);
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+
+	if (!validateName(body.name)) {
+		return response.status(400).json({
+			error: 'name must be unique'
+		});
+	}
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: generateUniqId()
+	};
+
+	persons = persons.concat(person);
+	response.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
