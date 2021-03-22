bf7942ffe020cfd3ee4b224b1da38cd3ab7092a8
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-03-22 12:30:46 +0200

    add DB

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
-let persons = [
-	{
-		name: 'Arto Hellas',
-		number: '234234234234',
-		id: 1
-	},
-	{
-		name: 'Dan Abramov',
-		number: '12 - 43 - 234345',
-		id: 2
-	},
-	{
-		name: 'Mary Poppendieck',
-		number: '39 - 23 - 6423122',
-		id: 3
-	},
-	{
-		name: 'Ada Lovelace',
-		number: '0649238374',
-		id: 4
-	}
-];
+const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 
@@ -32,66 +12,26 @@ morgan.token('content', function (request, response) {
 
 app.use(express.static('build'));
 app.use(express.json());
+app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] :content'));
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons);
+	Person.find({}).then((persons) => {
+		response.json(persons);
+	});
 });
-
-app.get('/info', (request, response) => {
-	const amount = persons.length;
-	const date = new Date();
-
-	response.send(`<p>Phonebook has info for ${amount} people.</p>
-<p>${date}</p>`);
-});
-
-app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const person = persons.find((person) => person.id === id);
-
-	if (!person) {
-		return response.status(400).end();
-	}
-
-	response.json(person);
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((person) => person.id !== id);
-
-	response.status(204).end();
-});
-
-const generateUniqId = () => {
-	const ids = persons.map((person) => person.id);
-	let id = Math.floor(Math.random() * 1000000);
-	while (ids.includes(id)) {
-		id++;
-	}
-	return id;
-};
-
-const validateName = (name) => name && !persons.map((person) => person.name).includes(name);
 
 app.post('/api/persons', (request, response) => {
 	const body = request.body;
 
-	if (!validateName(body.name)) {
-		return response.status(400).json({
-			error: 'name must be unique'
-		});
-	}
-
-	const person = {
+	const person = new Person({
 		name: body.name,
-		number: body.number,
-		id: generateUniqId()
-	};
+		number: body.number
+	});
 
-	persons = persons.concat(person);
-	response.json(person);
+	person.save().then((savedPerson) => {
+		response.json(person);
+	});
 });
 
 const unknownEndpoint = (request, response) => {
@@ -100,7 +40,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT ${PORT}`);
 });
