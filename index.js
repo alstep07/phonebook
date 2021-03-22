e13602a1fe76d11bf23f089fb9b00c8fc632d3e2
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-03-22 18:06:44 +0200

    add delete method

--- index.js
+++ index.js
@@ -3,22 +3,51 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/person');
+const { isValidObjectId } = require('mongoose');
+const { response } = require('express');
 
 const app = express();
-
-morgan.token('content', function (request, response) {
-	return JSON.stringify(request.body);
-});
 
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
+
+morgan.token('content', function (request, response) {
+	return JSON.stringify(request.body);
+});
 app.use(morgan(':method :url :status :res[content-length] :content'));
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.log(error.message);
+	if (error.name === 'CastError') {
+		return response.status(404).send({ error: 'malformatted id' });
+	}
+
+	next(error);
+};
+app.use(errorHandler);
 
 app.get('/api/persons', (request, response) => {
 	Person.find({}).then((persons) => {
 		response.json(persons);
 	});
+});
+
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((err) => next(err));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -34,11 +63,13 @@ app.post('/api/persons', (request, response) => {
 	});
 });
 
-const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' });
-};
-
-app.use(unknownEndpoint);
+app.delete('api/persons/:id', (request, response) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((err) => next(err));
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
