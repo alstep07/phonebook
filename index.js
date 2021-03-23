ca11bb4dd950a2b25ed259a35ed19fb86f471049
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-03-23 15:09:32 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const Person = require('./models/person');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
-app.use(express.static('build'));
-app.use(express.json());
-app.use(cors());
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
 
-morgan.token('content', function (request, response) {
-	return JSON.stringify(request.body);
-});
-app.use(morgan(':method :url :status :res[content-length] :content'));
+morgan.token('content', function (request) {
+  return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] :content'))
 
 app.get('/api/persons', (request, response) => {
-	Person.find({}).then((persons) => {
-		response.json(persons);
-	});
-});
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
-	Person.findById(request.params.id)
-		.then((person) => {
-			if (person) {
-				response.json(person);
-			} else {
-				response.status(404).end();
-			}
-		})
-		.catch((err) => next(err));
-});
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((err) => next(err))
+})
 
 app.post('/api/persons', (request, response, next) => {
-	const body = request.body;
+  const body = request.body
 
-	const person = new Person({
-		name: body.name,
-		number: body.number
-	});
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-	person
-		.save()
-		.then((savedPerson) => {
-			response.json(savedPerson);
-		})
-		.catch((err) => next(err));
-});
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((err) => next(err))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
-	Person.findByIdAndRemove(request.params.id)
-		.then((result) => {
-			response.status(204).end();
-		})
-		.catch((err) => next(err));
-});
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((err) => next(err))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-	const body = request.body;
+  const body = request.body
 
-	const person = {
-		name: body.name,
-		number: body.number
-	};
+  const person = {
+    name: body.name,
+    number: body.number
+  }
 
-	Person.findByIdAndUpdate(request.params.id, person, { new: true })
-		.then((updatedPerson) => {
-			response.json(updatedPerson);
-		})
-		.catch((error) => next(error));
-});
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
 
 const errorHandler = (error, request, response, next) => {
-	console.log(error.name, error.message);
-	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' });
-	} else if (error.name === 'ValidationError') {
-		return response.status(400).json({ error: error.message });
-	}
+  console.log(error.name, error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-	next(error);
-};
-app.use(errorHandler);
+  next(error)
+}
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server is running on PORT ${PORT}`);
-});
+  console.log(`Server is running on PORT ${PORT}`)
+})
